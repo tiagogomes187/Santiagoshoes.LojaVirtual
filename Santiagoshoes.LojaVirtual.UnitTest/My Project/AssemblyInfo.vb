76cb40fa78f19805457bcac78eb6b5b72c0dc0b5
TIAGO GOMES <santiagocat1936@hotmail.com>
2018-12-08 22:14:44 -0200

    Initial commit

---
+++ Santiagoshoes.LojaVirtual.UnitTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("Santiagoshoes.LojaVirtual.UnitTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Santiagoshoes.LojaVirtual.UnitTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("52df3eab-1138-4773-a1b2-a33668bf29ba")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ santiagoshoes.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="santiagoshoes.LojaVirtual.Web.MvcApplication" Language="C#" %>
