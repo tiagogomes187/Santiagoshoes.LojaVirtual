ec8dc9f230aded8066c3e7a5fbfb7543109edf71
TIAGO GOMES <santiagocat1936@hotmail.com>
2018-12-10 21:18:39 -0200

    Classe Produto

---
+++ Santiagoshoes.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Santiagoshoes.LojaVirtual.Dominio.Entidade
+{
+    class Produto
+    {
+        public int ProdutoId { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Descricao { get; set; }
+
+        public Decimal Preco { get; set; }
+
+        public String Categoria { get; set; }
+    }
+}
